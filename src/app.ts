63aa27155f0d2a30099c92ee31390d2b0f0eb9ee
K31Sonawane <44919541+K31Sonawane@users.noreply.github.com>
2026-02-05 12:30:04 +0530

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -2,11 +2,10 @@ import express from "express";
 import graphRoutes from "./routes/graph.routes";
 import cors from "cors";
 
-
 const app = express();
 app.use(express.json());
 app.use(cors({
-  origin: "https://insightboard-frontend-idaf.vercel.app/", // frontend URL
+  origin: "https://insightboard-frontend-idaf.vercel.app/",
 }));
 app.get("/", (req, res) => {
   res.send("InsightBoard API is running 🚀");
